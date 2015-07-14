58c42669a6aee3206912f80ef2a3999dd66315a5
Rion Dooley <dooley@tacc.utexas.edu>
2015-07-14 08:51:38 -0500

    Updating entrypoint to standardize environment for startup using compose. Reverting back to default ports.

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,35 +5,42 @@ if [[ -f "/app/config/environment.sh" ]]; then
 else
   echo "No environment config present in /app/config/environment.sh. Reading from container environment"
 fi
+
 if [[ -z "$MYSQL_HOST" ]]; then
   MYSQL_HOST=mysql
 fi
 
-if [[ -n "MYSQL_PORT_3306_TCP_PORT" ]]; then
+if [[ -n "$MYSQL_PORT_3306_TCP_PORT" ]]; then
   MYSQL_PORT=3306
 elif [[ -z "$MYSQL_PORT" ]]; then
   MYSQL_PORT=3306
 fi
 
-if [[ -z "$MYSQL_ENV_MYSQL_DATABASE" ]]; then
-  MYSQL_ENV_MYSQL_DATABASE=agave
+if [[ -n "$MYSQL_ENV_MYSQL_USERNAME" ]]; then
+  MYSQL_USERNAME=$MYSQL_ENV_MYSQL_USERNAME
+elif [[ -z "$MYSQL_USERNAME" ]]; then
+  MYSQL_USERNAME=agaveuser
 fi
 
-if [[ -z "$MYSQL_ENV_MYSQL_PASSWORD" ]]; then
-  MYSQL_ENV_MYSQL_PASSWORD=password
+if [[ -n "$MYSQL_ENV_MYSQL_PASSWORD" ]]; then
+  MYSQL_PASSWORD=$MYSQL_ENV_MYSQL_PASSWORD
+elif [[ -z "$MYSQL_PASSWORD" ]]; then
+  MYSQL_PASSWORD=password
 fi
 
-if [[ -z "$MYSQL_ENV_MYSQL_DATABASE" ]]; then
-  MYSQL_ENV_MYSQL_DATABASE=agave
+if [[ -n "$MYSQL_ENV_MYSQL_DATABASE" ]]; then
+  MYSQL_DATABASE=$MYSQL_ENV_MYSQL_DATABASE
+elif [[ -z "$MYSQL_DATABASE" ]]; then
+  MYSQL_DATABASE=agave-api
 fi
 
 for i in /usr/local/tomcat/conf/context.xml;
 do
   sed -i -e "s/%MYSQL_HOST%/$MYSQL_HOST/" $i
   sed -i -e "s/%MYSQL_PORT%/$MYSQL_PORT/" $i
-  sed -i -e "s/%MYSQL_USERNAME%/$MYSQL_ENV_MYSQL_DATABASE/" $i
-  sed -i -e "s/%MYSQL_PASSWORD%/$MYSQL_ENV_MYSQL_PASSWORD/" $i
-  sed -i -e "s/%MYSQL_DATABASE%/$MYSQL_ENV_MYSQL_DATABASE/" $i
+  sed -i -e "s/%MYSQL_USERNAME%/$MYSQL_USERNAME/" $i
+  sed -i -e "s/%MYSQL_PASSWORD%/$MYSQL_PASSWORD/" $i
+  sed -i -e "s/%MYSQL_DATABASE%/$MYSQL_DATABASE/" $i
 done
 
 #service rsyslog start
